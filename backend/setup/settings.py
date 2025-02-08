6bfb4c45a7a8c2371fba9fe581d212fbe562b459
Paulo Davi <araujo.paulo.davi@gmail.com>
2025-02-08 19:23:14 -0300

    feat: add dockerfile

--- backend/setup/settings.py
+++ backend/setup/settings.py
@@ -1,11 +1,8 @@
 import os
 from pathlib import Path
-from dotenv import load_dotenv
 from django.contrib.messages import constants as messages
 
-load_dotenv()
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# Build paths inside the project like this: BASE_DIR / "subdir".
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
@@ -57,8 +54,12 @@ WSGI_APPLICATION = "setup.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": "db",
+        "PORT": 5432,
     }
 }
 
@@ -104,6 +105,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Messages
 MESSAGE_TAGS = {
-    messages.ERROR: 'danger',
-    messages.SUCCESS: 'success'
+    messages.ERROR: "danger",
+    messages.SUCCESS: "success"
 }
