614edd6115c2d28a0d1c91d146556c64e883260b
Paulo Davi <paulo.araujo@cobli.co>
2025-02-08 14:59:57 -0300

    feat: add users

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from apps.users.views import home
+from apps.users.views import home, login
 
 urlpatterns = [
     path("", home, name="home"),
+    path("login", login, name="login"),
 ]
--- backend/apps/users/views.py
+++ backend/apps/users/views.py
@@ -1,4 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 def home(request):
+    if not request.user.is_authenticated:
+        messages.error(request, 'Usúario não esta logado')
+        return redirect('login')
+
     return render(request, "users/home.html")
+
+def login(request):
+    return render(request, "users/login.html")
