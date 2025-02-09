76467ede3d1aae4d73dd9299fe148d4cb69454b0
Paulo Davi <paulo.araujo@cobli.co>
2025-02-09 00:02:51 -0300

    feat: add ssl

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 python manage.py migrate --noinput
-python manage.py shell < ./create_superuser.py || true
 python manage.py collectstatic --noinput
-python manage.py migrate --noinput
+python manage.py shell < ./create_superuser.py || true
 
 exec "$@"
--- backend/setup/settings.py
+++ backend/setup/settings.py
@@ -108,3 +108,7 @@ MESSAGE_TAGS = {
     messages.ERROR: "danger",
     messages.SUCCESS: "success"
 }
+
+# Secure
+SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
