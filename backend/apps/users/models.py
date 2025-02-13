c6cf966a805ecaa1122f11ffcade36da22bbe4ec
Paulo Davi <paulo.araujo@cobli.co>
2025-02-12 22:08:34 -0300

    feat: add friend management

--- backend/apps/users/models.py
+++ backend/apps/users/models.py
@@ -31,6 +31,34 @@ class User(AbstractUser):
     def __str__(self):
         return self.username
 
+
+class FriendshipStatus(models.TextChoices):
+    PENDING = "PENDING", _("Pending")
+    ACCEPTED = "ACCEPTED", _("Accepted")
+    REJECTED = "REJECTED", _("Rejected")
+
+class Friendship(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user1 = models.ForeignKey(User, related_name="friendships1", on_delete=models.CASCADE)
+    user2 = models.ForeignKey(User, related_name="friendships2", on_delete=models.CASCADE)
+    requestd_by = models.ForeignKey(User, related_name="friendship_requests", on_delete=models.CASCADE)
+    status = models.CharField(
+        max_length=10,
+        choices=FriendshipStatus.choices,
+        default=FriendshipStatus.PENDING,
+        verbose_name=_("Status")
+    )
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_("Criado em"))
+    updated_at = models.DateTimeField(auto_now=True, verbose_name=_("Atualizado em"))
+
+    class Meta:
+        verbose_name = _("Amizade")
+        verbose_name_plural = _("Amizades")
+        unique_together = ["user1", "user2"]
+
+    def __str__(self):
+        return f"{self.user1.username} e {self.user2.username}"
+
 class Messages(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     sender = models.ForeignKey(User, related_name="sent_messages", on_delete=models.CASCADE)
